9c8093c823ef816346f058e5ae29bf8e9aad9550
Scotty <scotty@Scottys-MBP.home>
2018-04-01 02:09:46 -0400

    saving

---
+++ MvcPlayground/wwwroot/json/pallets.js
@@ -0,0 +1,57 @@
+const PALLETS = {
+    core: {
+        data: [
+            {
+                id: "001",
+                parent: "#",
+                text: "Pallet 1"
+            }, {
+                id: "002",
+                parent: "#",
+                text: "Pallet 2"
+            }, {
+                id: "003",
+                parent: "#",
+                text: "Pallet 3"
+            }, {
+                id: "004",
+                parent: "#",
+                text: "Pallet 4"
+            }, {
+                id: "005",
+                parent: "#",
+                text: "Pallet 5"
+            }, {
+                id: "006",
+                parent: "#",
+                text: "Pallet 6"
+            }, {
+                id: "007",
+                parent: "#",
+                text: "Pallet 7"
+            }, {
+                id: "008",
+                parent: "#",
+                text: "Pallet 8"
+            }, {
+                id: "009",
+                parent: "#",
+                text: "Pallet 9"
+            }, {
+                id: "031",
+                parent: "002",
+                text: "Box 1"
+            }, {
+                id: "311",
+                parent: "031",
+                text: "Item 1"
+            }, {
+                id: "021",
+                parent: "002",
+                text: "Box 2"
+            }
+        ],
+        multiple: false,
+        animation: 0
+    }
+};
